48b7e6c38c77ddb2bd2488efeee22afeb13745e2
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-02-18 21:31:04 +0100

    Added react-router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "../scss/App.scss";
-// import FrontScreen from "../components/FrontScreen";
+
+import FrontScreen from "./main/FrontScreen";
 import MainContainer from "./main/MainContainer";
 
-const App = <MainContainer />;
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={FrontScreen} />
+          <Route path="/main" component={MainContainer} />
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;

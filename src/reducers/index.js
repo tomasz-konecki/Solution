8c5d6f9b571037bc79c7a31d5428a5d1cb665d62
Michał Kostecki <michal.kostecki@billennium.pl>
2018-02-20 11:08:37 +0100

    removed dasherized names

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,3 @@
-export * from "./auth-reducer";
-export * from "./projects-reducer";
-export * from "./workers-reducer";
+export * from "./authReducer";
+export * from "./projectsReducer";
+export * from "./workersReducer";

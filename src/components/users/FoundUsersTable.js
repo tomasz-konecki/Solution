b86e75044e8bb8ec5aabce7c3015f44010cade66
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-03-02 13:29:45 +0100

    Corrected code

--- src/components/users/FoundUsersTable.js
+++ src/components/users/FoundUsersTable.js
@@ -3,29 +3,11 @@ import "../../scss/components/users/FoundUsersTable.scss";
 import uuid from "uuid";
 
 const FoundUsersTable = props => {
-  const createList = property =>
-    props.foundUsers.map((item, index) => (
-      <li key={index}>{item[property]}</li>
-    ));
-
-  const firstNames = createList("firstName");
-  const lastNames = createList("lastName");
-  const emails = createList("email");
-
-  const styles = {
-    display: "inline-block",
-    padding: "0 20px 0 0"
-  };
-
   const tableData = props.foundUsers.map((item, index) => (
-    <tr
-      key={uuid.v4()}
-      onClick={() => {
-        console.log("Clicked!");
-      }}
-    >
+    <tr key={uuid.v4()}>
       <td key={uuid.v4()}>{item.firstName}</td>
       <td key={uuid.v4()}>{item.lastName}</td>
+      <td key={uuid.v4()}>{item.id}</td>
     </tr>
   ));
 

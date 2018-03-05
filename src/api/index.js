64da069f157a88b834c80f634480d49f29f92ebb
Michał Kostecki <vfioox@gmail.com>
2018-03-05 12:31:46 +0100

    created a mock api

--- src/api/index.js
+++ src/api/index.js
@@ -165,4 +165,273 @@ class DCMTWebApi {
   // }
 }
 
-export default new DCMTWebApi();
+const UsersObject = {
+  "results": [
+  {
+  "id": "Desperados",
+  "firstName": "Antonio",
+  "lastName": "Banderas",
+  "role": null,
+  "isDeleted": false,
+  "email": "zonororo@nana.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "jgndfkjgdfs",
+  "firstName": "Basia",
+  "lastName": "Banderas",
+  "role": null,
+  "isDeleted": false,
+  "email": "basiabanda@nana.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "stringyjkjkllut",
+  "firstName": "Mikołaj",
+  "lastName": "Butkownik",
+  "role": null,
+  "isDeleted": false,
+  "email": "Butkowymifrancuski@sksa.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "programmer",
+  "firstName": "Siszarp",
+  "lastName": "Ceplusplus",
+  "role": null,
+  "isDeleted": false,
+  "email": "cc@mail.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "ide",
+  "firstName": "wizualstudjo",
+  "lastName": "codeblokzzz",
+  "role": null,
+  "isDeleted": false,
+  "email": "wizcode@mail.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "csharp",
+  "firstName": "csharp",
+  "lastName": "csharp",
+  "role": null,
+  "isDeleted": false,
+  "email": "csharp@csharp.csharp",
+  "phoneNumber": null
+  },
+  {
+  "id": "jfkskajd21",
+  "firstName": "Testownik",
+  "lastName": "Czterystacztery",
+  "role": null,
+  "isDeleted": false,
+  "email": "error100404@populacjaeuro.ru",
+  "phoneNumber": null
+  },
+  {
+  "id": "fdomurad",
+  "firstName": "Fabian",
+  "lastName": "Domurad",
+  "role": null,
+  "isDeleted": false,
+  "email": "fabian.domurad@billennium.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "prezydent",
+  "firstName": "Adndrzej",
+  "lastName": "Duda",
+  "role": null,
+  "isDeleted": false,
+  "email": "Duda@nana.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "pescobar",
+  "firstName": "Pablo",
+  "lastName": "Escobar",
+  "role": null,
+  "isDeleted": false,
+  "email": "columbia@ciamail.com",
+  "phoneNumber": null
+  },
+  {
+  "id": "database",
+  "firstName": "majeskuel",
+  "lastName": "eskuelserwer",
+  "role": null,
+  "isDeleted": false,
+  "email": "me@mail.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "jfkskafghfjd21",
+  "firstName": "Radosław",
+  "lastName": "Goniący",
+  "role": null,
+  "isDeleted": false,
+  "email": "radekgoniacypilke@pilkadonogi.me",
+  "phoneNumber": null
+  },
+  {
+  "id": "mjackson",
+  "firstName": "Michał",
+  "lastName": "Jackson",
+  "role": null,
+  "isDeleted": false,
+  "email": "mjackson@mockmail.com",
+  "phoneNumber": null
+  },
+  {
+  "id": "json",
+  "firstName": "json",
+  "lastName": "json",
+  "role": null,
+  "isDeleted": false,
+  "email": "json@json.json",
+  "phoneNumber": null
+  },
+  {
+  "id": "Donald",
+  "firstName": "Donald",
+  "lastName": "Kaczor",
+  "role": null,
+  "isDeleted": false,
+  "email": "DonaldKaczor@nana.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "prezes",
+  "firstName": "Jarosław",
+  "lastName": "Kaczyński",
+  "role": null,
+  "isDeleted": false,
+  "email": "Juro@nana.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "jkowalski",
+  "firstName": "Jan",
+  "lastName": "Kowalski",
+  "role": null,
+  "isDeleted": false,
+  "email": "jan.kowalski@mail.com",
+  "phoneNumber": null
+  },
+  {
+  "id": "id",
+  "firstName": "firstName",
+  "lastName": "lastName",
+  "role": null,
+  "isDeleted": false,
+  "email": "email@mail.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "marcin",
+  "firstName": "marcin",
+  "lastName": "marcin",
+  "role": null,
+  "isDeleted": false,
+  "email": "marcindlu993@gmail.com",
+  "phoneNumber": null
+  },
+  {
+  "id": "emelo",
+  "firstName": "Elo",
+  "lastName": "Melo",
+  "role": null,
+  "isDeleted": false,
+  "email": "zero@elomelo.com",
+  "phoneNumber": null
+  },
+  {
+  "id": "Krzysztof",
+  "firstName": "Krzysztof",
+  "lastName": "Nanna",
+  "role": null,
+  "isDeleted": false,
+  "email": "mamamiak@billennium.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "stringjkl",
+  "firstName": "Anna",
+  "lastName": "Nowak",
+  "role": null,
+  "isDeleted": false,
+  "email": "annnnnnanoooowaaaak@o222.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "stringyut",
+  "firstName": "Joanna",
+  "lastName": "Nowocińska",
+  "role": null,
+  "isDeleted": false,
+  "email": "annnanoooowaak@o220002.pl",
+  "phoneNumber": null
+  },
+  {
+  "id": "wolczak",
+  "firstName": "Wojciech",
+  "lastName": "Olczak",
+  "role": null,
+  "isDeleted": false,
+  "email": "testEmail@mail.mock",
+  "phoneNumber": null
+  },
+  {
+  "id": "stringyjklut",
+  "firstName": "Mariusz",
+  "lastName": "Piłosz",
+  "role": null,
+  "isDeleted": false,
+  "email": "polowmariusz123121@o0002.pl",
+  "phoneNumber": null
+  }
+  ],
+  "currentPage": 1,
+  "totalPageCount": 2
+  };
+
+class DCMTMockApi extends DCMTWebApi {
+  auth(username, password) {
+    return Promise.resolve({
+      "email": "jane.doe@kappa.com",
+      "extra": "Jane Doe"
+    });
+  }
+
+  getUsers() {
+    return Promise.resolve({
+      data: UsersObject
+    });
+  }
+
+  searchAD(user){
+    return Promise.resolve({
+      data: [
+        {
+          firstName: user,
+          lastName: "Kowalski",
+          email: user + "@kowal.pl",
+          phoneNumber: "554663252",
+          id: user + "kowalski"
+        },
+        {
+          firstName: user + "2",
+          lastName: "Kowalski",
+          email: user + "@kowal.pl",
+          phoneNumber: "554663252",
+          id: user + "kowalski2"
+        }
+      ]
+    });
+  }
+}
+
+
+export default new DCMTMockApi();

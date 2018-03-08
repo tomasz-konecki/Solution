fdd14c32253b2426086120c26c0fe31ae9204692
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-03-08 16:02:21 +0100

    Added SmothTable component

---
+++ src/components/common/SmoothTable.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import Icon from "./Icon";
+
+class SmoothTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      construct: props.construct,
+      data: props.data
+    };
+  }
+  deepenFunction(func, ...args) {
+    return () => func(...args);
+  }
+  generateToolBox(object, toolBoxColumn) {
+    return toolBoxColumn.toolBox.map((button, index) => (
+      <button
+        key={button.icon.icon}
+        onClick={this.deepenFunction(button.click, object)}
+      >
+        <Icon {...button.icon} />
+      </button>
+    ));
+  }
+  generateLegend() {
+    return this.state.construct.columns.map((column, index) => (
+      <th key={column.field + index}>{column.pretty}</th>
+    ));
+  }
+  generateRow(object) {
+    const { construct } = this.state;
+    return (
+      <tr
+        key={object[construct.keyField]}
+        className={
+          "smooth-row" +
+          (construct.rowClass !== undefined ? " " + construct.rowClass : "")
+        }
+      >
+        {construct.columns.map((column, index) => {
+          if (column.field !== undefined)
+            return (
+              <td
+                key={column.field}
+                className="smooth-cell"
+                style={{ width: column.width + "%" }}
+              >
+                {object[column.field]}
+              </td>
+            );
+          else if (column.toolBox !== undefined)
+            return (
+              <td
+                key="____toolBox"
+                className="smooth-cell"
+                style={{ width: column.width + "%" }}
+              >
+                {this.generateToolBox(object, column)}
+              </td>
+            );
+        })}
+      </tr>
+    );
+  }
+  render() {
+    const { construct } = this.state;
+    const list = this.state.data.map((object, index) =>
+      this.generateRow(object)
+    );
+    return (
+      <table
+        className={
+          "smooth-table" +
+          (construct.tableClass !== undefined ? " " + construct.tableClass : "")
+        }
+      >
+        <thead>
+          <tr className="smooth-row">{this.generateLegend()}</tr>
+        </thead>
+        <tbody>{list}</tbody>
+      </table>
+    );
+  }
+}
+
+export default SmoothTable;
--- src/components/users/UsersList.js
+++ src/components/users/UsersList.js
@@ -1,27 +1,33 @@
 import React, { Component } from "react";
 import Icon from "../common/Icon";
+import SmoothTable from "../common/SmoothTable";
 
 const UsersList = props => {
-  console.log("USERS LIST: ", props.users);
+  const construct = {
+    rowClass: "user-block",
+    tableClass: "users-list-container",
+    keyField: "id",
+    columns: [
+      { width: 20, field: "firstName", pretty: "Imię" },
+      { width: 30, field: "lastName", pretty: "Nazwisko" },
+      { width: 30, field: "email", pretty: "Email" },
+      { width: 19, field: "phoneNumber", pretty: "Telefon" },
+      {
+        width: 1,
+        toolBox: [
+          { icon: { icon: "times" }, click: () => {} },
+          {
+            icon: { icon: "edit", iconType: "far" },
+            click: object => {
+              alert(object.firstName);
+            }
+          }
+        ]
+      }
+    ]
+  };
 
-  const usersList = props.users.map((item, index) => (
-    <div key={item.id} className="smooth-row user-block">
-      <div className="smooth-cell smooth-2x">{item.firstName}</div>
-      <div className="smooth-cell smooth-3x">{item.lastName}</div>
-      <div className="smooth-cell smooth-1x">{item.email}</div>
-      <div className="smooth-cell smooth-1x">{item.phoneNumber}</div>
-      <div className="smooth-cell smooth-0x">
-        <button>
-          <Icon icon="times" />
-        </button>
-        <button>
-          <Icon iconType="far" icon="edit" />
-        </button>
-      </div>
-    </div>
-  ));
-
-  return <div className="smooth-table users-list-container">{usersList}</div>;
+  return <SmoothTable construct={construct} data={props.users} />;
 };
 
 export default UsersList;

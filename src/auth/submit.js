d41e2dea10ba122a69010d1ed691b502964137eb
Michał Kostecki <vfioox@gmail.com>
2018-04-24 14:16:41 +0200

    added no-script handling

--- src/auth/submit.js
+++ src/auth/submit.js
@@ -11,7 +11,7 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const errorHandler = dispatch => error => {
   dispatch(authStop());
-  if (error.response.data.errorOccured === true) {
+  if (error.response !== undefined && error.response.data.errorOccured === true) {
     const { errors } = error.response.data;
     throw new SubmissionError({
       _error: errors[Object.keys(errors)[0]]
@@ -34,7 +34,6 @@ const submit = ({ username, password }, dispatch) => {
     .then(() => dispatch(authStart()))
     .then(() => DCMTWebApi.auth(username, password))
     .then(userBlock => {
-      console.log('we have a success');
       dispatch(authSuccess(userBlock));
       dispatch(authStop());
       dispatch(push("/main"));

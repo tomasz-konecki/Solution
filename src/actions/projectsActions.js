5bb08b8ca4ae2464e935ffc698e816da7811d2fc
Adrian Polubinski <adrian.polubinski@billennium.com>
2018-06-21 07:18:44 +0200

    Test

--- src/actions/projectsActions.js
+++ src/actions/projectsActions.js
@@ -19,7 +19,6 @@ export const loadProjects = (page = 1, limit = 25, other = {}) => {
     dispatch(asyncStarted());
     WebApi.projects.post.list(settings)
       .then(response => {
-        console.log(response.data);
         if(!response.errorOccurred()) {
           dispatch(loadProjectsSuccess(response.extractData()));
         }
